8360cd2dd6d1c009bf57b81e9e0fec2818dfdb4b
Verenasworld <verena.felizidas@gmail.com>
2019-09-07 21:00:57 +0200

    Bmi Rechner hinzugefügt

--- js/main.js
+++ js/main.js
@@ -1,2 +1,30 @@
 
 
+
+// Formel: bmi = körpermasse / (körperhöhe in meter) ^
+
+let heightObj = document.getElementById("height");
+let weightObj = document.getElementById("weight");
+
+let calculateBmi = function() {
+    let height = parseFloat(heightObj.value.replace(",", "."));
+    let weight = parseFloat(weightObj.value);
+    console.log(height);
+    let bmi = (weight / (height * height));
+
+    let result = Math.round(bmi * 10) / 10;
+
+    let resultObj = document.getElementById("result");
+    resultObj.innerText = ("" + result).replace(".", ",");
+};
+
+// let button = document.getElementById("calculate");
+// button.addEventListener("click", calculateBmi);
+
+
+heightObj.addEventListener("change", calculateBmi);
+weightObj.addEventListener("change", calculateBmi);
+heightObj.addEventListener("keyup", calculateBmi);
+weightObj.addEventListener("keyup", calculateBmi);
+
+
