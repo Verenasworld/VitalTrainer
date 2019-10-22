84dc3fe973fc57412caad8b21ed6e1f84dd50f06
Verenasworld <verena.felizidas@gmail.com>
2019-10-22 21:37:47 +0200

    add scrollToTop2

---
+++ js/goToTop.js
@@ -0,0 +1,17 @@
+//sanftes Hochscrollen
+
+document.getElementById("top").addEventListener("click", hochscrollen);
+function hochscrollen() {
+    var scrolling = setInterval(
+        function () {
+            window.scrollBy(0, -20);
+
+            if (document.documentElement.scrollTop == 0) {
+                window.clearInterval(scrolling);
+            }
+
+        },
+        10);
+
+
+}
