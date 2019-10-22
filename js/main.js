019f5fce1acc7d962e5ef13e35a7f1367934b18d
Verenasworld <verena.felizidas@gmail.com>
2019-10-22 21:35:51 +0200

    add scrollToTop

--- js/main.js
+++ js/main.js
@@ -1,30 +1,28 @@
 
-
-
 // Formel: bmi = körpermasse / (körperhöhe in meter) ^
 
-let heightObj = document.getElementById("height");
-let weightObj = document.getElementById("weight");
+    let heightObj = document.getElementById("height");
+    let weightObj = document.getElementById("weight");//todo
 
-let calculateBmi = function() {
-    let height = parseFloat(heightObj.value.replace(",", "."));
-    let weight = parseFloat(weightObj.value);
-    console.log(height);
-    let bmi = (weight / (height * height));
+    let calculateBmi = function() {
+        let height = parseFloat(heightObj.value.replace(",", "."));
+        let weight = parseFloat(weightObj.value);
+        console.log(height);
+        let bmi = (weight / (height * height));
 
-    let result = Math.round(bmi * 10) / 10;
+        let result = Math.round(bmi * 10) / 10;
 
-    let resultObj = document.getElementById("result");
-    resultObj.innerText = ("" + result).replace(".", ",");
-};
+        let resultObj = document.getElementById("result");
+        resultObj.innerText = ("" + result).replace(".", ",");
+    };
 
-// let button = document.getElementById("calculate");
-// button.addEventListener("click", calculateBmi);
+    // let button = document.getElementById("calculate");
+    // button.addEventListener("click", calculateBmi);
 
 
-heightObj.addEventListener("change", calculateBmi);
-weightObj.addEventListener("change", calculateBmi);
-heightObj.addEventListener("keyup", calculateBmi);
-weightObj.addEventListener("keyup", calculateBmi);
+    heightObj.addEventListener("change", calculateBmi);//todo kaqki
+    weightObj.addEventListener("change", calculateBmi);
+    heightObj.addEventListener("keyup", calculateBmi);
+    weightObj.addEventListener("keyup", calculateBmi);
 
 
