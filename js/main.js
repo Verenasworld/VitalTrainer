bef46029e7ea251ce3b5efd57509d9148fbf9ccb
Verenasworld <verena.felizidas@gmail.com>
2019-10-29 10:31:45 +0100

    do the todo list ( nav and Bmi ) Bmi calculator change Content

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 // Formel: bmi = körpermasse / (körperhöhe in meter) ^
 
     let heightObj = document.getElementById("height");
-    let weightObj = document.getElementById("weight");//todo
+    let weightObj = document.getElementById("weight");
 
     let calculateBmi = function() {
         let height = parseFloat(heightObj.value.replace(",", "."));
@@ -14,16 +14,31 @@
 
         let resultObj = document.getElementById("result");
         resultObj.innerText = ("" + result).replace(".", ",");
+
+        document.getElementById("below").style.display = "none";
+        document.getElementById("normal").style.display = "none";
+        document.getElementById("above").style.display = "none";
+
+        if (bmi < 18.5){
+            document.getElementById("below").style.display =" block" ;
+        } else if (bmi > 18.5 && bmi < 25){
+            document.getElementById("normal").style.display = " block";
+        } else if (!isNaN(bmi)){
+            document.getElementById("above").style.display = " block";
+        }
+
+
     };
+
 
     // let button = document.getElementById("calculate");
     // button.addEventListener("click", calculateBmi);
 
 
-    heightObj.addEventListener("change", calculateBmi);//todo kaqki
+    heightObj.addEventListener("change", calculateBmi);
     weightObj.addEventListener("change", calculateBmi);
     heightObj.addEventListener("keyup", calculateBmi);
     weightObj.addEventListener("keyup", calculateBmi);
 
 
-//TODO BMI und Nav verlinken das Nav zu BMI springt  !
+
